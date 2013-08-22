0ef7ad66b6625b7dbabd78ee01042eedcbd83f98
Christer Edvartsen <cogo@starzinger.net>
2013-08-22 15:05:42 +0200

    Added Application class

---
+++ src/ImboLauncher/Application.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * This file is part of the ImboLauncher package
+ *
+ * (c) Christer Edvartsen <cogo@starzinger.net>
+ *
+ * For the full copyright and license information, please view the LICENSE file that was
+ * distributed with this source code.
+ */
+
+namespace ImboLauncher;
+
+use ImboLauncher\Command;
+
+/**
+ * Main application class
+ *
+ * @author Christer Edvartsen <cogo@starzinger.net>
+ * @package Application
+ */
+class Application extends Symfony\Component\Console\Application {
+    /**
+     * Class constructor
+     */
+    public function __construct() {
+        parent::__construct('ImboLauncher', 'dev');
+
+        // Register commands
+        $this->addCommands(array(
+            new Command\StartServers(),
+            new Command\KillServers(),
+        ));
+    }
+}
